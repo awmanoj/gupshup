0e19274bec93336b11f635932198ef540386b24b
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-07-08 19:15:35 +0530

    implement room logic

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	go r.run()
 
 	log.Println("starting the gupshup server")
 
