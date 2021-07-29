d0c0ff0854fd78325d75ddd980bf077d87b598d3
Manoj Awasthi <awasthi.manoj@gmail.com>
2021-07-29 22:37:26 +0530

    Add auth

---
+++ auth.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+
+type authHandler struct {
+	next http.Handler
+}
+
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+		// not authenticated handler
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	} else if err != nil {
+		// some other error
+		panic(err.Error())
+	} else {
+		// success - call next handler
+		h.next.ServeHTTP(w, r)
+	}
+}
+
+func MustAuth(handler http.Handler) http.Handler {
+	return &authHandler{next: handler}
+}
--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/room", r)
 	go r.run()
 
